1146c4385d343874e28a0522c95bea1399730003
Sundar Nambuvel <sunprema@gmail.com>
2022-08-08 14:05:14 -0500

    ant d changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React,{useState} from "react";
+import { Button, DatePicker, Space, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+import moment from 'moment';
+
+const App = () => {
+
+  const [startDate, setStartDate] = useState(moment('2015-01-01', 'YYYY-MM-DD'));
+  const showDate = () => {
+    alert( startDate );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Go places!</h1>
+      <Space>
+      <DatePicker defaultValue={startDate} onChange={(newDate) => setStartDate(newDate) }/>
+        <Button type="primary" onClick={ (e) => showDate()} > Primary Button</Button>
+      </Space>
     </div>
   );
-}
+};
 
 export default App;
